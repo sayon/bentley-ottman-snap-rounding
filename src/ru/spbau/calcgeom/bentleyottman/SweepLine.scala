697e793b02b735a082f740f1e3a28cf2bd47ac3b
Sayon <igorjirkov@gmail.com>
2014-03-22 20:50:31 +0400

    Sweepline refactoring

--- src/ru/spbau/calcgeom/bentleyottman/SweepLine.scala
+++ src/ru/spbau/calcgeom/bentleyottman/SweepLine.scala
@@ -31,22 +31,28 @@ class SweepLine {
     }
 
   def cross(seg: Segment) = {
-    buffer add seg
+    buffer = buffer add seg
   }
 
   def uncross(seg: Segment) = {
-    buffer remove seg
+    buffer = buffer remove seg
+  }
+
+  private[this] def orderedPair(fst: Segment, snd: Segment) = {
+    val b = buffer.asInstanceOf[Node[Segment]]
+    (b find fst, b find snd) match {
+      case (f: Node[Segment], s: Node[Segment]) if f.findNext == s => (f.key, s.key)
+      case (f: Node[Segment], s: Node[Segment]) if s.findNext == f => (s.key, f.key)
+      case _ => throw new IllegalStateException("Can not determine which one is above, which one is below!")
+    }
   }
 
   def handleIntersection(fst: Segment, snd: Segment): (Segment, Segment) = {
 
     val b = buffer.asInstanceOf[Node[Segment]]
     b.swap(fst, snd)
-    (b find fst, b find snd) match {
-      case (f: Node[Segment], s: Node[Segment]) if f.findNext == s => (f.key, s.key)
-      case (f: Node[Segment], s: Node[Segment]) if s.findNext == f => (s.key, f.key)
-      case _ => throw new IllegalStateException("Can not determine which one is above, which one is below!")
-    }
+
+    orderedPair(fst, snd)
   }
 
 }
